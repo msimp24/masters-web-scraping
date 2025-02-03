11fdaf95add6a4249434d3d8301342553eaad498
Mark Simpson <mark.simpson4@gmail.com>
2025-02-03 14:57:50 +0000

    pga-golf-scraper

--- insert-tournament-data.py
+++ insert-tournament-data.py
@@ -11,7 +11,7 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 sql = "INSERT INTO tournaments(tournament_id, name, year, location) VALUES (%s, %s, %s, %s)"
-val = ("401703492", "Farmers Insurance", "2025", "La Jolla, CA")
+val = ("401703498", "Arnold Palmer Invitational", "2025", "Orlando, Fl")
 
 mycursor.execute(sql, val)
 
--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ import pandas as pd
 import mysql.connector
 from sqlalchemy import create_engine
 
+# Creates database connection
 mydb = mysql.connector.connect(
  host="localhost",
  port=3307,
@@ -12,34 +13,23 @@ mydb = mysql.connector.connect(
  db="pga-data",
 )
 
+mycursor = mydb.cursor()
+
+#creates new database engine
 engine = create_engine("mysql+mysqlconnector://admin:Pass1234@localhost:3307/pga-data")
 
+
+#bypasses website firewall header
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.9",
     "Referer": "https://www.google.com/",
 }
-
-def get_tournament_status(headers, tournamentId):
-  url = f"https://www.espn.co.uk/golf/leaderboard/_/tournamentId/{tournamentId}"
-  response = requests.get(url, headers=headers)
-
-  if response.status_code == 200:
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    div = soup.find('div', class_='status cf mt4 mb4')
-
-    span_element = div.find('span')
-
-    text = span_element.get_text()
-
-    return text
   
-def get_last_id():
-  mycursor = mydb.cursor()
+#function that gets the current id of the tournament  
+def get_tournament_id():
 
-  mycursor.execute("SELECT MAX(tournament_id) FROM tournaments")
+  mycursor.execute("select last_tournament_id from tournament_tracker")
 
   myresult = mycursor.fetchone()
   tournament_id = myresult[0]
@@ -48,12 +38,22 @@ def get_last_id():
 
 def scrape_data_to_database(headers):
   
-    tournamentId = get_last_id()
+    isNewWeek = True
+  
+    tournamentId = get_tournament_id()
   
     url = f"https://www.espn.co.uk/golf/leaderboard/_/tournamentId/{tournamentId}"
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
+      
+      soup = BeautifulSoup(response.text, 'html.parser')
+    
+      div = soup.find('div', class_='status cf mt4 mb4')
+
+      span_element = div.find('span')
+
+      status = span_element.get_text()      
 
       soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -71,16 +71,34 @@ def scrape_data_to_database(headers):
         
 
       else:
+        isNewWeek = False
         print("table not found, the structure might have changed")  
 
     else:
       print('Failed to retrieve the page, Status code: {respsonse.status_code}')
-
-    columns = ['tournament_id', 'Position', 'Player', 'Score','Today','Thru','R1', 'R2', 'R3', 'R4', 'Total']
-
-    df = pd.DataFrame(rows, columns = columns)
+      
+    print
+      
+    if(status == 'Final'):
+      columns = ['tournament_id', 'Position', 'Player', 'Score', 'R1', 'R2', 'R3', 'R4', 'Total', 'Earnings', 'Fedex Pts']  
+      
+      newTournamentId = tournamentId + 1
+      print(newTournamentId)
+      mycursor.execute("UPDATE tournament_tracker SET last_tournament_id = %s" , (newTournamentId,))
+      mydb.commit()
+      
+      df = pd.DataFrame(rows, columns = columns)
+      df.to_sql('final_leaderboard', engine,if_exists='replace', index=False)
+      
+    elif(isNewWeek):
+      
+      columns = ['tournament_id', 'Position', 'Player', 'Score','Today','Thru','R1', 'R2', 'R3', 'R4', 'Total']      
+      df = pd.DataFrame(rows, columns = columns)
+      df.to_sql('live_leaderboard', engine,if_exists='replace', index=False)
     
-    df.to_sql('leaderboard', engine,if_exists='replace', index=False)
+    else:
+      
+      print('New Tournament Data table has not yet been added to the website to be scraped')
 
 
 scrape_data_to_database(headers)
